4e5356cc09cb294b6a0c03081076d7df4a787288
kimjunseo <andy55233@gmail.com>
2023-03-26 13:19:32 +0900

    spring initializr

---
+++ src/main/kotlin/com/kyungdyu/shoppingmall/ShoppingmallApplication.kt
@@ -0,0 +1,11 @@
+package com.kyungdyu.shoppingmall
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoppingmallApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShoppingmallApplication>(*args)
+}
